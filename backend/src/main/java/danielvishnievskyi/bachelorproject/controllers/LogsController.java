fe16fd73cfecbe1e99a88207ff17580d110dd1bd
dv121ns <daniel.vishnievskyi@student.tuke.sk>
2023-05-03 23:40:29 +0200

    pageable logs

--- backend/src/main/java/danielvishnievskyi/bachelorproject/controllers/LogsController.java
+++ backend/src/main/java/danielvishnievskyi/bachelorproject/controllers/LogsController.java
@@ -8,12 +8,19 @@ import org.javers.core.Javers;
 import org.javers.repository.jql.QueryBuilder;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RestController
 @PreAuthorize("hasAnyRole('ADMIN_VIEW')")
@@ -23,7 +30,10 @@ public class LogsController {
   private final Javers javers;
 
   @GetMapping()
-  public ResponseEntity<?> getLogs(@PageableDefault Pageable pageable) {
+  public ResponseEntity<?> getLogs(
+    @PageableDefault(sort = "id", direction = DESC) Pageable pageable,
+    @RequestParam(required = false) String filter
+  ) {
     QueryBuilder jql = QueryBuilder.byClass(
       Location.class,
       Building.class,
@@ -32,8 +42,15 @@ public class LogsController {
 
     var changes = javers.findChanges(jql.build());
 
-    var page = new PageImpl<>(changes, pageable, changes.size());
+    int start = (int) pageable.getOffset();
+    int end = Math.min((start + pageable.getPageSize()), changes.size());
 
-    return ResponseEntity.ok(javers.getJsonConverter().toJson(page));
+    var page = new PageImpl<>(changes.subList(start, end), pageable, changes.size());
+
+    Map<String, Object> response = new HashMap<>();
+    response.put("page", page);
+    response.put("totalPages", page.getTotalPages());
+
+    return ResponseEntity.ok(javers.getJsonConverter().toJson(response));
   }
 }
