d0bda27f7ceb4fe294202f77574077d04a295a9a
dv121ns <daniel.vishnievskyi@student.tuke.sk>
2023-05-07 14:06:48 +0200

    Fixed: undefined totalPaged number fixed

--- frontend/src/features/hooks/PageNavigation.jsx
+++ frontend/src/features/hooks/PageNavigation.jsx
@@ -9,7 +9,7 @@ export const PageNavigation = ({setPage, page, pageable}) => {
 				 onClick={() => setPage(pageable?.totalPages - 1 > page ? page + 1 : page)}></i>
 			<i className="bi bi-chevron-double-right" onClick={() => setPage(pageable?.totalPages - 1)}></i>
 			<span>
-         Page {page + 1} of {pageable.totalPages === 0 ? 1 : pageable.totalPages}
+         Page {page + 1} of {pageable?.totalPages === 0 ? 1 : pageable?.totalPages}
       </span>
 		</>
 	)
